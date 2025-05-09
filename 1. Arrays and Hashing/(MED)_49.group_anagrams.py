abec4ad096421dc2de5d391072e5dd5c5e4043e2
WilliamTay <102462963+WilliamTayNZ@users.noreply.github.com>
2025-05-10 01:03:50 +1200

    Added 49. Group Anagrams solution

---
+++ 1. Arrays and Hashing/(MED)_49.group_anagrams.py
@@ -0,0 +1,36 @@
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        first_string = strs[0]
+        anagram_groups = [[first_string]] # Initialises with 1 group containing the first string in the array
+
+        # Hashmap to map the sorted string (identifier) to the index of associated group in anagram_groups
+        anagram_to_group_dict = {}
+        anagram_to_group_dict[''.join(sorted(first_string))] = 0
+
+        for i in range(1,len(strs)): # For each remaining string in array
+            string = strs[i]
+            sorted_string = ''.join(sorted(string))
+
+            if sorted_string in anagram_to_group_dict:
+                group_index = anagram_to_group_dict[sorted_string]
+                anagram_groups[group_index].append(string)
+
+            else: # Create new group
+                anagram_to_group_dict[sorted_string] = len(anagram_groups)
+                anagram_groups.append([string])
+
+        return anagram_groups
+
+
+solution = Solution()
+
+print(solution.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+print(solution.groupAnagrams([[""]]))
+print(solution.groupAnagrams([["a"]]))
+print(solution.groupAnagrams([
+    "eat", "tea", "tan", "ate", "nat", "bat",
+    "tab", "bat", "tap", "pat", "apt",
+    "rat", "tar", "art", "star", "tars", "cheaters", "teachers", "hectares"
+]))
