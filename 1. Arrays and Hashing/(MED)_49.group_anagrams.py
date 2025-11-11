48ddcc4a20d9c5b45fb385ea451d9728045b847f
William Tay <102462963+WilliamTayNZ@users.noreply.github.com>
2025-11-11 14:11:13 +1300

    Update (MED)_49.group_anagrams.py

--- 1. Arrays and Hashing/(MED)_49.group_anagrams.py
+++ 1. Arrays and Hashing/(MED)_49.group_anagrams.py
@@ -1,6 +1,27 @@
 from typing import List
 
-class Solution:
+# Easier on the eyes
+class NewCleanerSolution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_groups = []
+
+        hashmap = {} # sorted_string -> original_string
+
+        for string in strs: # O(n)
+            sorted_string = ''.join(sorted(string)) # O(klogk)
+
+            if sorted_string in hashmap:
+                hashmap[sorted_string].append(string)
+            else:
+                hashmap[sorted_string] = [string]
+
+        for sorted_string in hashmap:
+            anagram_groups.append(hashmap[sorted_string])
+        
+        return anagram_groups
+
+# Slightly less constant space overhead because dicts store indices and not lists
+class OldSolution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         first_string = strs[0]
         anagram_groups = [[first_string]] # Initialises with 1 group containing the first string in the array
@@ -24,7 +45,7 @@ class Solution:
         return anagram_groups
 
 
-solution = Solution()
+solution = NewCleanerSolution()
 
 print(solution.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
 print(solution.groupAnagrams([[""]]))
