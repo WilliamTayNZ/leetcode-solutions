1fdf601a881ebdaf2ada747ee673310701fec4c9
William Tay <102462963+WilliamTayNZ@users.noreply.github.com>
2025-11-12 21:43:50 +1300

    Update (MED)_424.longest_repeating_character_replacement.py

--- 3. Sliding Window/(MED)_424.longest_repeating_character_replacement.py
+++ 3. Sliding Window/(MED)_424.longest_repeating_character_replacement.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
@@ -22,5 +20,6 @@ class Solution:
             longest_length = max(longest_length, r-l+1)
 
         return longest_length
+
 
 # November 12th, 2025
