a34de7e1d22b2c00d700ca9404c83ec081cbf25d
William Tay <102462963+WilliamTayNZ@users.noreply.github.com>
2025-11-12 15:24:12 +1300

    Update (MED)_347.top_k_frequent_elements_(bucketsort).py

--- 1. Arrays and Hashing/(MED)_347.top_k_frequent_elements_(bucketsort).py
+++ 1. Arrays and Hashing/(MED)_347.top_k_frequent_elements_(bucketsort).py
@@ -2,7 +2,7 @@ from typing import List
 
 from collections import Counter
 
-class Solution:
+class BucketSortSolution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
         counter = Counter(nums) # Creates key-value pairs num: frequency
@@ -25,6 +25,38 @@ class Solution:
                 # We can do this since it is guaranteed answer is unique
                 # Otherwise, multiple numbers could tie for frequency at the cutoff point, so we would need check len(result) >= k
                 return result
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums) # O(n)
+
+        frequency_to_elements_dict = {}
+
+        for key, value in counter.items(): # O(n)
+            if value in frequency_to_elements_dict:
+                frequency_to_elements_dict[value].append(key)
+            else:
+                frequency_to_elements_dict[value] = [key]
+
+        # print(frequency_to_elements_dict.keys())
+
+        frequency_list = list(frequency_to_elements_dict.keys()) # O(f), f <= n)
+        frequency_list.sort() # O(flogf), f <= n
+        frequency_list.reverse() # O(f)
+        # print(frequency_list)
+
+        keys_most_to_least_frequent = []
+
+        for i in range(len(frequency_list)): # O(n) in total
+            frequency = frequency_list[i]
+            elements = frequency_to_elements_dict[frequency]
+            for j in range(len(elements)):
+                keys_most_to_least_frequent.append(elements[j])
+                if len(keys_most_to_least_frequent) == k:
+                    return keys_most_to_least_frequent
+        
+        # O(n + flogf) time, worst case O(nlogn) time
+
             
 solution = Solution()
 print(solution.topKFrequent([1,1,1,2,2,3], 2))
