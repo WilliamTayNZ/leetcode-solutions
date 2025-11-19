157d747d9b348115991145e7a697a2204db529a4
William Tay <102462963+WilliamTayNZ@users.noreply.github.com>
2025-11-19 23:30:30 +1300

    Add files via upload

---
+++ 3. Sliding Window/(HARD)_239.sliding_window_maximum.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+class Solution: 
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        dq = deque()
+
+        # Initial window
+        for i in range(k):
+            while len(dq) > 0 and dq[-1] < nums[i]:
+                dq.pop()
+            dq.append(nums[i])
+
+        max_array = [dq[0]]
+        
+        # Start moving window
+        l = 1
+        r = k
+
+        while r < len(nums): 
+            if nums[l-1] == dq[0]:
+                dq.popleft()
+
+            while len(dq) > 0 and dq[-1] < nums[r]: 
+                dq.pop()
+            dq.append(nums[r])
+
+            max_array.append(dq[0])
+            l += 1
+            r += 1
+
+        return max_array
